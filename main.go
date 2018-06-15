2e4b386435cd641338c14feebc1fbe2bc4118992
Steve Huff <steve.huff@asics.com>
2018-06-15 17:11:43 -0400

    feat: healthcheck handler, cfg from env vars

--- main.go
+++ main.go
@@ -1,34 +1,85 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-  "github.com/thoas/stats"
-  "github.com/urfave/negroni"
+	"github.com/caarlos0/env"
+	"github.com/thoas/stats"
+	"github.com/urfave/negroni"
 )
 
+type config struct {
+	Port        int    `env:"PORT"`
+	Application string `env:"HEALTHCHECK_APP" envDefault:"healthcheck"`
+	Status      int    `env:"HEALTHCHECK_STATUS" envDefault:"200"`
+	Metrics     string `env:"HEALTHCHECK_METRICS" envDefault:"{}"`
+}
+
+type healthcheck struct {
+	Application string `json:"application"`
+	Status      int    `json:"status"`
+	Metrics     string `json:"metrics"`
+}
+
 func main() {
-  mux := http.NewServeMux()
+	cfg := config{}
+	hc := healthcheck{}
 
-  // middleware - stats
-  middlewareStats := stats.New()
-  mux.HandleFunc("/stats", statsHandler(middlewareStats))
+	mux := http.NewServeMux()
 
-  n := negroni.Classic()
-  n.UseHandler(mux)
+	n := negroni.Classic()
 
-  n.Run()
+	// middleware - config
+	n.Use(negroni.HandlerFunc(configHandler(&cfg)))
+
+	// middleware - stats
+	middlewareStats := stats.New()
+	mux.HandleFunc("/stats", statsHandler(middlewareStats))
+
+	// middleware - healthcheck
+	mux.HandleFunc("/healthcheck", healthcheckHandler(&hc, &cfg))
+
+	n.UseHandler(mux)
+
+	n.Run()
+}
+
+func healthcheckHandler(hc *healthcheck, c *config) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		hc.Application = c.Application
+		hc.Status = c.Status
+		hc.Metrics = c.Metrics
+
+		b, _ := json.Marshal(*hc)
+
+		w.Write(b)
+	}
 }
 
 func statsHandler(s *stats.Stats) func(http.ResponseWriter, *http.Request) {
-  return func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-TYpe", "application/json")
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 
-    stats := s.Data()
+		stats := s.Data()
 
-    b, _ := json.Marshal(stats)
+		b, _ := json.Marshal(stats)
 
-    w.Write(b)
-  }
+		w.Write(b)
+	}
+}
+
+func configHandler(c *config) func(http.ResponseWriter, *http.Request, http.HandlerFunc) {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		err := env.Parse(c)
+
+		if err != nil {
+			panic(fmt.Sprintf("panic: %s", err))
+		}
+
+		next(w, r)
+	}
 }
