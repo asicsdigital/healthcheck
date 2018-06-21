5cea5e19651bc2448ad8727436109cb1ae381e42
Steve Huff <steve.huff@asics.com>
2018-06-21 17:20:38 -0400

    feat: use ECS host IP to build CONSUL_HTTP_ADDR

--- scripts/envconsul_wrapper.sh
+++ scripts/envconsul_wrapper.sh
@@ -5,6 +5,13 @@ set -u
 DUMB_INIT=$(which dumb-init)
 ENVCONSUL=$(which envconsul)
 
+if [[ -z "$CONSUL_HTTP_ADDR" ]]; then
+  consul_ip_aws=$(curl -f -s http://169.254.169.254/latest/meta-data/local-ipv4)
+  CONSUL_IP=${consul_ip_aws:-127.0.0.1}
+  CONSUL_HTTP_ADDR="http://${CONSUL_IP}:8500"
+  export CONSUL_HTTP_ADDR
+fi
+
 $DUMB_INIT \
   $ENVCONSUL \
   $EXTRA_ARGS \
