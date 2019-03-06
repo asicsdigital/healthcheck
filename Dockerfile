bf3214cf96542c1008d91a7ff5269844dd58d8d1
Tim Hartmann <tfhartmann@gmail.com>
2019-03-06 11:52:38 -0500

    feat: makes the wrapper authenticate to vault
    The wrapper will now try to loginto vault with the default vault role of
    hc, get a token and if it can, export the token to the ENV, and start a
    version of envconsul that will look into vault for the honeycomb api
    secret.
    
    If we aren't running on AWS or the VAULT_TOKEN isn't passed in, start a
    version that tries to get it's configs from Consul and don't send events
    to Honeycomb (becasue don't have a API Token)

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,20 @@ RUN go get -d -v ./...
 RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
 RUN CGO_ENABLED=0 GOOS=linux go get -v github.com/asicsdigital/dudewheresmy
 
+# Download and verify the integrity of the download first
+FROM sethvargo/hashicorp-installer:0.1.3 AS installer
+ARG CONSUL_VERSION='1.4.0'
+ARG VAULT_VERSION='1.0.2'
+RUN /install-hashicorp-tool "vault" "$VAULT_VERSION"
+
 FROM hashicorp/envconsul:alpine
-RUN apk --no-cache add ca-certificates
+RUN apk --no-cache add ca-certificates bind-tools
 WORKDIR /root/
 COPY --from=0 /go/src/github.com/asicsdigital/healthcheck/app .
 RUN mkdir -p static
 COPY --from=0 /go/src/github.com/asicsdigital/healthcheck/static ./static
 COPY --from=0 /go/bin/dudewheresmy .
+COPY --from=installer /software/vault /usr/local/bin/vault
 
 ENV PORT=8080
 ENV CONSUL_PREFIX=healthcheck
--- scripts/envconsul_wrapper.sh
+++ scripts/envconsul_wrapper.sh
@@ -5,6 +5,7 @@ set -u
 DUDEWHERESMY=$(PATH=$PATH:. which dudewheresmy)
 DUMB_INIT=$(which dumb-init)
 ENVCONSUL=$(which envconsul)
+VAULT_ROLE="${VAULT_ROLE:-hc}"
 
 if [[ -z "$CONSUL_HTTP_ADDR" ]]; then
   CONSUL_IP=$($DUDEWHERESMY hostip)
@@ -13,9 +14,21 @@ if [[ -z "$CONSUL_HTTP_ADDR" ]]; then
 fi
 
 if [[ -z "$VAULT_ADDR" ]]; then
-  VAULT_IP=$($DUDEWHERESMY hostip)
-  VAULT_ADDR="http://${VAULT_IP}:8200"
+  CONSUL_IP=$($DUDEWHERESMY hostip)
+  VAULT_IP=$(dig +short active.vault.service.consul @${CONSUL_IP} -p 8600)
+  VAULT_PORT=$(dig -t srv +short active.vault.service.consul @${CONSUL_IP} -p 8600 | cut -d' ' -f3)
+  VAULT_ADDR="http://${VAULT_IP}:${VAULT_PORT}"
   export VAULT_ADDR
+fi
+
+if curl -fs -m 1 http://169.254.169.254/latest/meta-data/ -o /dev/null ; then
+  echo "Logging into Vault with role: ${VAULT_ROLE}"
+  vault login -no-print=true -method=aws role=${VAULT_ROLE}
+
+  if [ -f .vault-token ]; then
+       VAULT_TOKEN=$(cat .vault-token)
+       export VAULT_TOKEN
+  fi
 fi
 
 _HCL_CONFIG=$(cat<<EOT
@@ -45,3 +58,4 @@ else
       -config config.hcl \
       -exec=./app
 fi
+#  CONSUL_IP=$(./dudewheresmy ip) ; VAULT_IP=$(dig +short active.vault.service.consul @${CONSUL_IP} -p 8600); VAULT_PORT=$(dig -t srv +short active.vault.service.consul @${CONSUL_IP} -p 8600 | cut -d' ' -f3) ; VAULT_ADDR="http://${VAULT_IP}:${VAULT_PORT}"
